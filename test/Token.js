fa2dc3edd8d4147a3cbca71968442819c5acf07d
Ryan Fitzgerald <ryfitz@ymail.com>
2026-03-30 13:54:46 +1300

    4. token balances

--- test/Token.js
+++ test/Token.js
@@ -1,32 +1,42 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-const { deployTokenFixture } = require("./helpers/TokenFixtures")
+const { deployTokenFixture } = require("./helpers/TokenFixtures");
 
 const tokens = (n) => {
-  return ethers.parseUnits(n.toString(), 18)
-}
+  return ethers.parseUnits(n.toString(), 18);
+};
 
 describe("Token", () => {
+  const NAME = "Dapp Uni";
+  const SYMBOL = "DAPP";
+  const DECEMALS = "18";
+  const TOTALSUPPLY = tokens("1000000");
 
-  it("has correct name", async () =>{
-    const {token} = await loadFixture(deployTokenFixture)
-    expect(await token.name()).to.equal("Dapp Uni")
-  })
+  it("has correct name", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.name()).to.equal(NAME);
+  });
 
-  it("has correct symbol", async () =>{
-    const {token} = await loadFixture(deployTokenFixture)
-    expect(await token.symbol()).to.equal("DAPP")
-  })
+  it("has correct symbol", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.symbol()).to.equal(SYMBOL);
+  });
 
-  it("has correct decimals", async () =>{
-    const {token} = await loadFixture(deployTokenFixture)
-    expect(await token.decimals()).to.equal("18")
-  })
+  it("has correct decimals", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.decimals()).to.equal(DECEMALS);
+  });
 
-  it("has correct totalSupply", async () =>{
-    const {token} = await loadFixture(deployTokenFixture)
-    expect(await token.totalSupply()).to.equal(tokens("1000000"))
-  })
-}) 
+  it("has correct totalSupply", async () => {
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.totalSupply()).to.equal(TOTALSUPPLY);
+  });
+  it("assigns total supply to deployer", async () => {
+    const { token, deployer } = await loadFixture(deployTokenFixture);
+    expect(await token.balanceOf(deployer.address)).to.equal(TOTALSUPPLY);
+  });
+});
